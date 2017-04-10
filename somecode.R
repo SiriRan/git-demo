8aefa479346884900fece08140dc8e89fae92274
Siri Ranlund <siri.ranlund@kcl.ac.uk>
2017-04-10 10:07:03 +0100

    adding lm

--- somecode.R
+++ somecode.R
@@ -1,11 +1,10 @@
 # Poldrack workshop - Repro course
 
 # Website: https://github.com/poldrack/reproducible-workflows
-
-
 # In terminal: 
     # git init # open new github hidden file
     # git status # currently empty
+    # git log # check what you've done
 
 #rm(list = ls())
 #setwd('/Users/Siri/Desktop/Repro17/git-demo/')
@@ -15,4 +14,8 @@
 df=read.table('http://data.bris.ac.uk/datasets/swyt56qr4vaj17op9cw3sag7d/LskyetalPLOSONE.csv',
               header=TRUE,sep=',')
 head(df)
+
+lm.result=lm(conspiracist_avg~age,data=df)
+summary(lm.result)
+
 
