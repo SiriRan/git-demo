6a21ede45651d0409386dff1d46692b6e9b680e6
Siri Ranlund <siri.ranlund@kcl.ac.uk>
2017-04-10 10:23:53 +0100

    adding outlier removal

--- somecode.R
+++ somecode.R
@@ -18,4 +18,30 @@ head(df)
 lm.result=lm(conspiracist_avg~age,data=df)
 summary(lm.result)
 
+plot(df$age,df$conspiracist_avg)
+
+max_age=120
+min_age=18
+
+#stopifnot(max(df$age)<max_age)
+#stopifnot(min(df$age)>min_age)
+
+#max(df$age) #32757
+#min(df$age) # 5
+
+
+df=subset(df,age>min_age&age<max_age)
+
+stopifnot(max(df$age)<max_age)
+stopifnot(min(df$age)>min_age)
+
+max(df$age) #84
+min(df$age) # 19
+
+plot(df$age,df$conspiracist_avg)
+
+lm.result=lm(conspiracist_avg~age,data=df)
+summary(lm.result)
+
+
 
